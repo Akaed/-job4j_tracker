8666764652f7c4d6939d15e50168f5bb7dfd2a74
Ilya <ilyayurch@gmail.com>
2022-10-01 03:31:00 +0400

    Поля объекта

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,9 +1,26 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println(this.food + " " + this.name);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
     public static void main(String[] args) {
-        Cat polkan = new Cat();
-        Cat sharik = new Cat();
-        Cat zhychka = new Cat();
+        Cat gav = new Cat();
+        gav.giveNick("Petya");
+        gav.eat("Meat");
+        gav.show();
+
     }
 }
