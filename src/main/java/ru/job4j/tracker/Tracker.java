89e10728ab5276cc23da3a85d5476e601a225afe
Ilya <ilyayurch@gmail.com>
2022-10-13 19:48:33 +0400

    6. Метод замены заявки. Tracker.replace [#211748 #351212]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -65,5 +65,4 @@ public class Tracker {
         int index = indexOf(id);
         return index != -1 ? items[index] : null;
     }
-
 }
--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -75,4 +75,5 @@ public class TrackerTest {
         tracker.delete(id);
         assertThat(tracker.findById(id)).isNull();
     }
+
 }
