efb4b940a0b73baa506a07cff1f95521c29fd6ac
Ilya <ilyayurch@gmail.com>
2022-10-15 03:21:38 +0400

    3. Интерфейс Input [#181092]

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -8,12 +8,11 @@ public class MagicBall {
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
         int answer = new Random().nextInt(3);
-        if (answer == 0) {
-            System.out.println("Да");
-        } else if (answer == 1) {
-            System.out.println("Нет");
-        } else {
-            System.out.println("Может быть");
+
+            switch (name) {
+            case "0" -> System.out.println("Да");
+            case "1" -> System.out.println("Нет");
+            default -> System.out.println("Может быть");
         }
     }
 }
