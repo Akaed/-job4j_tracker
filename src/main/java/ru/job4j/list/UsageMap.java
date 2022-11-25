5a284da0274b1d01746ea338d7c95f4ec1329484
Ilya <ilyayurch@gmail.com>
2022-11-26 02:35:57 +0300

    0. Map, HashMap [#212652 #364160]

--- src/main/java/ru/job4j/list/UsageMap.java
+++ src/main/java/ru/job4j/list/UsageMap.java
@@ -6,6 +6,8 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
 
         for (String key : map.keySet()) {
             String value = map.get(key);
