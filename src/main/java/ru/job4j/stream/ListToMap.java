d845115ad3e61ea616be2b54d8c0454452e50151
Ilya <ilyayurch@gmail.com>
2022-12-26 16:34:07 +0300

    8. Преобразование List в Map. [#110226 #374404]

--- src/main/java/ru/job4j/stream/ListToMap.java
+++ src/main/java/ru/job4j/stream/ListToMap.java
@@ -11,7 +11,7 @@ public class ListToMap {
                 Collectors.toMap(
                         Student::getSurname,
                         student -> student,
-                        (name, score) -> name
+                        (oldObject, newObject) -> oldObject
                 )
         );
     }
